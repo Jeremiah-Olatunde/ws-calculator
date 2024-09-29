f724db7e4fea38b670a43f7fc7d650a28ba56610
Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2024-09-29 14:48:13 +0100

    feat(server): setup websocket server

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,24 @@
-console.log("Hello via Bun!");
+
+const server = Bun.serve({
+	fetch(request, server) {
+		if (server.upgrade(request)) {
+			return;
+		}
+
+		console.error("ws: upgrade failed");
+		return new Response(null, { status: 400, statusText: "Websocket upgrade failed" });
+	},
+	websocket: {
+		message(_ws, message) {
+			console.log("ws(message): message received ->", message);
+		},
+		open(_ws) {
+			console.log("ws(open): connection established");
+		},
+		close(_ws, _code, _reason) {
+			console.log("ws(close): connection closed");
+		},
+	}
+})
+
+console.log(`SERVER RUNNING |  hostname: ${server.hostname} | port: ${server.port}`);
