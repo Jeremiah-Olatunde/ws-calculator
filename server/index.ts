ac24b68ab01a3b0132879e5f9feb7c3542744e75
Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2024-09-29 15:41:06 +0100

    feat(server): add validation for incoming websocket messages

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,4 @@
+import { type Operation, SchemaOperation } from "./schema";
 
 const server = Bun.serve({
 	fetch(request, server) {
@@ -9,8 +10,33 @@ const server = Bun.serve({
 		return new Response(null, { status: 400, statusText: "Websocket upgrade failed" });
 	},
 	websocket: {
-		message(_ws, message) {
+		message(ws, message) {
 			console.log("ws(message): message received ->", message);
+
+
+			if(typeof message !== "string"){
+				console.error(`ws(message): buffers are not supported`);
+				return;
+			};
+
+			const rawJSON = safeParseJSON(message);
+
+			if(rawJSON === null){
+				console.error(`ws(message): invalid json`);
+				return;
+			}
+
+
+			const parsed = SchemaOperation.safeParse(rawJSON);
+
+			if(parsed.error){
+				console.error(`ws(message): invalid data`, parsed.error.errors);
+				return;
+			}
+
+			const operation: Operation = parsed.data;
+
+			console.log("ws(message):", operation);
 		},
 		open(_ws) {
 			console.log("ws(open): connection established");
@@ -22,3 +48,12 @@ const server = Bun.serve({
 })
 
 console.log(`SERVER RUNNING |  hostname: ${server.hostname} | port: ${server.port}`);
+
+
+const safeParseJSON = (x: string): unknown | null => {
+	try {
+		return JSON.parse(x);
+	} catch(error) {
+		return null;
+	}
+}
